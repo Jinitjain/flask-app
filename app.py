76e50eb9bbb656404c4d94fa6a0b01f7454cdfb4
Jinit Jain <jinitmjain15698@gmail.com>
2020-05-26 19:18:26 +0530

    Flask-Cors enabled

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import json
 import random 
 from flask import Flask, jsonify, request 
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from scrapping_modules.news_scrapper import extract_article 
 
 # creating the flask app 
 app = Flask(__name__) 
 # creating an API object 
-api = Api(app) 
+api = Api(app)
+CORS(app) 
   
 # extract relation between different entities 
 class extract_relation(Resource): 
